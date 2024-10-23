cfac01662fa30ca882acde11e453abc6aff0b9b9
AlonsoDalton <alphonnsos@gmail.com>
2024-10-23 18:49:40 -0300

    Add files via upload

---
+++ Untitled-1.swift
@@ -0,0 +1,7 @@
+//cálculo com operações simples
+var a = 12
+var b = 4
+print("a soma do", a, "com o", b, "=", a + b)
+print("a subtração do", a, "pelo", b, "=", a - b)
+print("a multiplicação do", a, "com o", b, "=", a * b)
+print("a divisão do", a, "pelo o", b, "=", a / b)
